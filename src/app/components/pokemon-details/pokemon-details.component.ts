0b1217f2b492da591b5ebea5721261abb4312f03
“AmroGFarghali” <lordmooora@gmail.com>
2025-03-08 00:24:43 +0200

    finished up until stats

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,23 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
-import { CommonModule, UpperCasePipe } from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 @Component({
   selector: 'app-pokemon-details',
-  imports: [CommonModule],
+  imports: [CommonModule, TitleCasePipe],
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.css',
 })
-export class PokemonDetailsComponent {
+export class PokemonDetailsComponent implements OnInit {
   pokemon: any;
+  pokemonSprite: string = '';
+  pokemonTypes: string[] = [];
+  species: any;
+  description: string = '';
+  getTypeClass(type: string) {
+    const typeColors: { [key: string]: string } = {
+      fire: 'bg-danger text-white',
+      water: 'bg-primary text-white',
+      grass: 'bg-success text-white',
+      electric: 'bg-warning text-dark',
+      psychic: 'bg-pink text-white',
+      ice: 'bg-info text-white',
+      dragon: 'bg-dark text-white',
+      normal: 'bg-secondary text-white',
+      fighting: 'bg-danger text-white',
+      flying: 'bg-info text-white',
+      poison: 'bg-purple text-white',
+      ground: 'bg-warning text-dark',
+      rock: 'bg-brown text-white',
+      bug: 'bg-success text-white',
+      ghost: 'bg-dark text-white',
+      steel: 'bg-secondary text-white',
+      dark: 'bg-dark text-white',
+      fairy: 'bg-light text-dark',
+    };
+    return typeColors[type] || 'bg-secondary text-white';
+  }
 
-  constructor(private route: ActivatedRoute, private pokemonService: PokemonService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id'); 
+    const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      this.pokemonService.getPokemonById(+id).subscribe((data) => {
-        this.pokemon = data;
+      this.pokemonService.getPokemonById(+id).subscribe({
+        next: (data: any) => {
+          this.pokemon = data.pokemon;
+          this.species = data.species;
+          this.pokemonSprite =
+            this.pokemon.sprites.other['official-artwork'].front_default;
+
+          this.pokemonTypes = this.pokemon.types.map((t: any) => t.type.name);
+          this.description = this.species['flavor_text_entries'][1][
+            'flavor_text'
+          ].replace(/[\n\f]/g, ' ');
+          console.log(this.species);
+        },
       });
     }
   }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
   private apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  private speciesUrl = 'https://pokeapi.co/api/v2/pokemon-species/';
 
   constructor(private http: HttpClient) {}
 
-  getPokemonById(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}${id}`);
+  // getPokemonById(id: number): Observable<any> {
+  //   return this.http.get(`${this.apiUrl}${id}`);
+  // }
+  getPokemonById(id: number) {
+    //it doesnt work if i need to return two requests in an object?
+    const species = this.http.get(`${this.speciesUrl}${id}`);
+    const pokeData = this.http.get(`${this.apiUrl}${id}`);
+    return forkJoin({ pokemon: pokeData, species: species });
   }
 }
