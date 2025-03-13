33ceb41abea8910fec5973592664147f5ae71060
Marwa Elgazar <marwaelgazar1@gmail.com>
2025-03-13 22:24:06 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PokemonCartComponent } from './components/cart/cart.component';
 import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/notFound/not-found.component';
 import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -13,4 +14,5 @@ export const routes: Routes = [
   { path: 'cart', component: PokemonCartComponent },
   { path: 'about', component: AboutComponent },
   { path: 'pokemon/:id', component: PokemonDetailsComponent }, // Dynamic route
+  { path: '**', component: NotFoundComponent },
 ];
