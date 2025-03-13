d9d2b3ab27e7e315f17f4234c794ab39ec41cca4
Habiba samir <habibas244@gmail.com>
2025-03-14 01:10:09 +0200

    navBar changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PokemonCartComponent } from './components/cart/cart.component';
 import { AboutComponent } from './components/about/about.component';
-import { NotFoundComponent } from './components/notFound/not-found.component';
 import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-import { NotFoundComponent } from './components/NotFoundComponent/not-found.component';
 import { AuthLayOutComponent } from './layout/auth-lay-out/auth-lay-out.component';
 import { HomeLayOutComponent } from './layout/home-lay-out/home-lay-out.component';
+import { NotFoundComponent } from './components/NotFoundComponent/not-found.component';
 
 export const routes: Routes = [
   {path:"", component:AuthLayOutComponent,children:[
